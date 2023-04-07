3cf14032576c8c29251a005a457fff88fdc9574a
Maurício Braz Gonçalves <82123955+mauriciobraz@users.noreply.github.com>
2023-04-07 11:40:13 -0400

    fix(client): remove useless formatter

--- apps/client/src/locales/formatters.ts
+++ apps/client/src/locales/formatters.ts
@@ -1,11 +1,5 @@
-import { time } from 'discord.js';
-
 import type { Formatters, Locales } from './i18n-types';
 
 export function initFormatters(locale: Locales): Formatters {
-  return {
-    dRelativeTime(value) {
-      return time(value, 'D');
-    },
-  };
+  return {};
 }
